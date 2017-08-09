0b033bf70e1e7f1036ce35638bc91105236315fb
Michelle Kidd <michekidd@gmail.com>
2017-08-09 00:53:31 -0500

    working

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+////////EDIT!!!////////// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use("/", routes);
+
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
